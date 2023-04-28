642b8205ce3412c6d7f389a74bc6bcfdb66ed08b
Luis Henrique Gomes Zortea <102925017+LuisHZortea@users.noreply.github.com>
2023-04-28 08:44:40 -0300

    Update App.js

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import { StatusBar } from 'expo-status-bar';
 import { useEffect, useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import estilo from './src/filmes/styles';
 
 export default function App() {
   const [filmes, setFilmes] = useState([]);
@@ -17,72 +18,22 @@ export default function App() {
   }, []);
 
   return (
-    <View style={styles.container}>
-      <View style={styles.header}>
-        <Text style={styles.headerText}>Filmes</Text>
+    <View style={estilo.container}>
+      <View style={estilo.header}>
+        <Text style={estilo.headerText}>Filmes</Text>
       </View>
       {filmes.length > 0 ? (
         filmes.map((filme) => (
-          <View key={filme._id} style={styles.filmeContainer}>
-            <Text style={styles.titulo}>{filme.attributes.titulo}</Text>
-            <Text style={styles.subtitulo}>{filme.attributes.subtitulo}</Text>
-            <Text style={styles.sinopse}>{filme.attributes.sinopse}</Text>
+          <View key={filme._id} style={estilo.filmeContainer}>
+            <Text style={estilo.titulo}>{filme.attributes.titulo}</Text>
+            <Text style={estilo.subtitulo}>{filme.attributes.subtitulo}</Text>
+            <Text style={estilo.sinopse}>{filme.attributes.sinopse}</Text>
           </View>
         ))
       ) : (
-        <Text style={styles.loading}>Carregando...</Text>
+        <Text style={estilo.loading}>Carregando...</Text>
       )}
       <StatusBar style="auto" />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#5c453e',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  header: {
-    backgroundColor: '#5c453e',
-    paddingVertical: 20,
-    paddingHorizontal: 30,
-    borderBottomWidth: 1,
-    borderBottomColor: '#f0f0f0',
-    marginBottom: 20,
-  },
-  headerText: {
-    fontSize: 24,
-    fontWeight: 'bold',
-    color: '#f0f0f0',
-  },
-  filmeContainer: {
-    backgroundColor: '#f0f0f0',
-    borderRadius: 10,
-    padding: 20,
-    marginBottom: 20,
-    maxWidth: '80%',
-  },
-  titulo: {
-    fontSize: 18,
-    fontWeight: 'bold',
-    marginBottom: 10,
-    color: '#5c453e',
-  },
-  subtitulo: {
-    fontSize: 15,
-    fontWeight: 'bold',
-    marginBottom: 10,
-    color: '#5c453e',
-  },
-  sinopse: {
-    fontSize: 16,
-    color: '#5c453e',
-  },
-  loading: {
-    fontSize: 18,
-    fontWeight: 'bold',
-    color: '#f0f0f0',
-  },
-});
